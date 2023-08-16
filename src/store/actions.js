9f5347f13259a88878d1939aa88f8cb690d85db4
mumo-esther <mumoesther2021@gmail.com>
2023-08-16 17:06:24 +0300

    Fetch greetings from backend endpoint

---
+++ src/store/actions.js
@@ -0,0 +1,10 @@
+export const fetchRandomGreeting = () => async (dispatch) => {
+  try {
+    const response = await fetch("http://127.0.0.1:3000/greetings/random");
+    const data = await response.json();
+    console.log(response);
+    dispatch({ type: "SET_RANDOM_GREETING", payload: data.greeting });
+  } catch (error) {
+    console.error("Error fetching greeting:", error);
+  }
+};
